da7c21424968c2650ecaa9a22a4ef7a1eebe8fb1
amitm144 <87026850+amitm144@users.noreply.github.com>
2024-03-12 11:11:01 +0200

    fix bill problem by autowired the value via constructor

--- src/main/java/il/ac/afeka/energyservice/utils/ConsumptionCalculator.java
+++ src/main/java/il/ac/afeka/energyservice/utils/ConsumptionCalculator.java
@@ -1,13 +1,20 @@
 package il.ac.afeka.energyservice.utils;
 
 import il.ac.afeka.energyservice.data.DeviceEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Component
 public class ConsumptionCalculator {
-    @Value("${house.power.price}")
     private static float PRICE_PER_WATT;
+
+    @Autowired
+    public ConsumptionCalculator(@Value("${house.power.price}") float pricePerWatt) {
+        PRICE_PER_WATT = pricePerWatt;
+    }
 
     public static Mono<Float> calculateTotalConsumption(Flux<DeviceEntity> devices) {
         return devices
